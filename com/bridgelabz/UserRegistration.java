069e6484033f622923a90b1fa57c3fb09720953d
MANOJ-51 <m4anoj51@gmail.com>
2022-06-29 18:47:49 +0530

    added: method to create first name of user and validation through regex

--- com/bridgelabz/UserRegistration.java
+++ com/bridgelabz/UserRegistration.java
@@ -1,7 +1,27 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    //taking scanner for user input
+    static Scanner scanner = new Scanner(System.in);
+
+    //main method
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
+
+        firstNameValidation();   //calling firstnameValidation
+    }
+
+    //validating first name of user using regex
+    public static void firstNameValidation() {
+        System.out.println("Enter the First Name starting letter should be capital ");
+        String firstName = scanner.nextLine();            //talking input from the user
+        String firstNameRegex = "[A-z[a-z]]{3,}";         //regex validation
+        Pattern pattern = Pattern.compile(firstNameRegex);  //compiling
+        Matcher matcher = pattern.matcher(firstName);       //matching
+        System.out.println("First Name pattern matching : " + matcher.matches());    //printing
     }
 }
